a5d55108789a4841fb97ea560cd99323ac1c3fe6
szymonczaplak <szymonsmok33@gmail.com>
2018-03-05 21:58:44 +0100

    Kurr... znaczy w koncu sie udalo <3

--- lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -1,23 +1,27 @@
 //
 // Created by maciejo on 04.03.18.
 //
-#include <iostream>
 #include <cstdint>
 #include <string>
 #include "DoubleBasePalindromes.h"
 
-bool is_palindrome(int str_int) {
-    std::string str = std::to_string(str_int);
-    unsigned long dl_wyrazu = str.length();
-    for (int i = 0; i < dl_wyrazu/2; i++) {
-        if (str[i] != str[dl_wyrazu - i -1]) {
-            return false;
-        }
+std::string  convertDecimalToBinaryString(int n)
+{
+    std::string binary="";
+    std::string str_i;
+    int remainder, i = 0;
+    while (n!=0)
+    {
+        remainder = n%2;
+        n /= 2;
+        str_i = std::to_string(remainder);
+        binary += str_i[0];
+        i++;
     }
-    return true;
+    return  binary;
 }
 
-bool is_palindrome_for_string(std::string str) {
+bool is_palindrome_for_str(std::string str) {
     int dl_wyrazu = str.length();
     for (int i = 0; i < dl_wyrazu/2; i++) {
         if (str[i] != str[dl_wyrazu - i -1]) {
@@ -27,20 +31,19 @@ bool is_palindrome_for_string(std::string str) {
     return true;
 }
 
-bool is_binary_palindrome(int str_int) {
-    int i=0;
-    std::string tab;
-    while(str_int)
-    {
-        tab[i++]=str_int%2;
-        str_int/=2;
-    }
-    return is_palindrome_for_string(tab);
+bool is_palindrome(int  number) {
+    std::string str = std::to_string(number);
+    return is_palindrome_for_str(str);
+}
+
+bool is_binary_palindrome(int number) {
+    std::string  binary = convertDecimalToBinaryString(number);
+    return is_palindrome_for_str(binary);
 }
 
 uint64_t DoubleBasePalindromes(int max_vaule_exculsive){
-    uint64_t suma = 0;
-    for(int i = 0; i < max_vaule_exculsive; i++)
+    uint64_t  suma = 0;
+    for(int i = 0; i <= max_vaule_exculsive; i++)
     {
         if(is_palindrome(i) && is_binary_palindrome(i))
         {
