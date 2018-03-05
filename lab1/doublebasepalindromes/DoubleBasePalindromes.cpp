05223925d71570d357a79faa27c4c872581ee7a9
MaciejKutyla <maciejo117@interia.pl>
2018-03-05 19:19:47 +0100

    hej

--- lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -1,13 +1,23 @@
 //
 // Created by maciejo on 04.03.18.
 //
-
+#include <iostream>
 #include <cstdint>
 #include <string>
 #include "DoubleBasePalindromes.h"
 
 bool is_palindrome(int str_int) {
     std::string str = std::to_string(str_int);
+    unsigned long dl_wyrazu = str.length();
+    for (int i = 0; i < dl_wyrazu/2; i++) {
+        if (str[i] != str[dl_wyrazu - i -1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool is_palindrome_for_string(std::string str) {
     int dl_wyrazu = str.length();
     for (int i = 0; i < dl_wyrazu/2; i++) {
         if (str[i] != str[dl_wyrazu - i -1]) {
@@ -18,33 +28,23 @@ bool is_palindrome(int str_int) {
 }
 
 bool is_binary_palindrome(int str_int) {
-    std::string binary;
-    int bin;
-    int i = 0;
-    while (str_int > 0) {
-
-        bin = str_int % 2;
-
-        binary[i] = bin;
-        str_int /= 2;
-        i++;
+    int i=0;
+    std::string tab;
+    while(str_int)
+    {
+        tab[i++]=str_int%2;
+        str_int/=2;
     }
-    int dl_wyrazu = binary.length();
-    for (int i = 0; i < dl_wyrazu/2; i++) {
-        if (binary[i] != binary[dl_wyrazu - i -1]) {
-            return false;
-        }
-    }
-    return true;
+    return is_palindrome_for_string(tab);
 }
 
 uint64_t DoubleBasePalindromes(int max_vaule_exculsive){
-    int suma = 0;
+    uint64_t suma = 0;
     for(int i = 0; i < max_vaule_exculsive; i++)
     {
         if(is_palindrome(i) && is_binary_palindrome(i))
         {
-            suma =+ i;
+            suma += i;
         }
     }
     return suma;
